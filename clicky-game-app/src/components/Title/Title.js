589fa2a63e61b2c3856f66b8847da60fe4f0f30f
Christopher Watson <christopher_watson@icloud.com>
2018-06-19 09:28:23 -0400

    score buttons

--- clicky-game-app/src/components/Title/Title.js
+++ clicky-game-app/src/components/Title/Title.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./Title.css";
 
-const Title = props => <h1 className="title">{props.children}</h1>;
+const Title = props => (
+  <header>
+    <h1 className="title">{props.children}</h1>
+    <div className="score-container">
+      <button type="button" className="btn btn-success" disabled>Top Score:
+        <span className="badge badge-light" id="win-num">5</span>
+      </button>
+      <button type="button" className="btn btn-dark" disabled>Losses:
+        <span className="badge badge-light" id="loss-num">5</span>
+      </button>
+    </div>
+  </header>
+);
 
 export default Title;
