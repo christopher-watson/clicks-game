c9d3b58c4e4476f93d84d6273d7ee6b4c95f69f6
Christopher Watson <christopher_watson@icloud.com>
2018-06-23 17:38:41 -0400

    not working

--- clicky-game-app/src/components/Title/Title.js
+++ clicky-game-app/src/components/Title/Title.js
@@ -6,10 +6,10 @@ const Title = props => (
     <h1 className="title">{props.children}</h1>
     <div className="score-container">
       <button type="button" className="btn btn-success" disabled>Top Score:
-        <span className="badge badge-light" id="win-num">5</span>
+        <span className="badge badge-light" id="top-score">5</span>
       </button>
-      <button type="button" className="btn btn-dark" disabled>Losses:
-        <span className="badge badge-light" id="loss-num">5</span>
+      <button type="button" className="btn btn-primary" disabled>Game Score:
+        <span className="badge badge-light" id="current-score">5</span>
       </button>
     </div>
   </header>
